fce660ba76bbda791f4182d2d4570d051a568d4e
Linisac Wu <linisac@yahoo.com.tw>
2018-04-12 00:29:16 -0700

    2 tests added

--- HW/HW1/ARITHinterpreter.hs
+++ HW/HW1/ARITHinterpreter.hs
@@ -1,43 +1,54 @@
---divide
-dvd :: Int -> Int -> Int
-dvd m n = if n == 0 then -1 else quot m n
+--(integer) division, m divided by n
+dvs :: Int -> Int -> Int
+dvs m n = if n == 0 then -1 else quot m n
 
---power
+--power (exponential), m^n
 pow :: Int -> Int -> Int
 pow m n
  | n < 0  = -1
  | n == 0 = 1
  | n > 0  = m * (pow m (n - 1))
- 
---factorial
+
+--factorial, n!
 fac :: Int -> Int
 fac n
  | n < 0  = -1
  | n == 0 = 1
  | n > 0  = n * (fac (n - 1))
 
---binomial
+--binomial coefficient, m choose n
 bin :: Int -> Int -> Int
 bin m n
  | m < 0  = -1
  | m == 0 = if n == 0 then 1 else 0
  | m > 0  = if (n >= 0 && n <= m) then (fac m) `quot` ((fac (m - n)) * (fac n)) else -1
 
-data ARITH = Num Int | Add ARITH ARITH | Sub ARITH ARITH | Mul ARITH ARITH | Div ARITH ARITH | Exp ARITH ARITH | Fac ARITH | Bin ARITH ARITH
 
+--AST of ARITH
+data ARITH = Num Int | Add ARITH ARITH | Sub ARITH ARITH | Mul ARITH ARITH
+            | Div ARITH ARITH | Exp ARITH ARITH | Fac ARITH | Bin ARITH ARITH
+
+--evaluation of ARITH
 eval :: ARITH -> Int
 eval (Num n)     = n
 eval (Add e1 e2) = (eval e1) + (eval e2)
 eval (Mul e1 e2) = (eval e1) * (eval e2)
 eval (Sub e1 e2) = (eval e1) - (eval e2)
-eval (Div e1 e2) = (eval e1) `dvd` (eval e2)
+eval (Div e1 e2) = (eval e1) `dvs` (eval e2)
 eval (Exp e1 e2) = pow (eval e1) (eval e2)
 eval (Fac e)     = fac (eval e) 
 eval (Bin e1 e2) = bin (eval e1) (eval e2)
 
-test_eval :: Bool
-test_eval = let e = Mul (Add (Num 3) (Num 5)) (Num 2) in
-            (eval e) == 16
-			
+--test 1 of eval: 2! ^ 4 * ((7 + 1 - 5) choose (6 / 3))
+test1_eval :: Bool
+test1_eval = let e = Mul (Exp (Fac (Num 2)) (Num 4)) (Bin (Sub (Add (Num 7) (Num 1)) (Num 5)) (Div (Num 6) (Num 3))) in
+            (eval e) == 48
+
+--test 2 of eval: 8 ^ 7 - 6! + C(5 * 3,  4 / 2 - 1)
+test2_eval :: Bool
+test2_eval = let e = Add (Sub (Exp (Num 8) (Num 7)) (Fac (Num 6))) (Bin (Mul (Num 5) (Num 3)) (Sub (Div (Num 4) (Num 2)) (Num 1))) in
+            (eval e) == 2096447
+
+--how ARITH expressions are reducted and printed on the screen under GHCi mode
 instance Show ARITH where
 	show e = show (eval e)
