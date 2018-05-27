0a347880383d92e40e080f67a758ebae9a25efeb
Linisac Wu <linisac@yahoo.com.tw>
2018-05-26 23:33:19 -0700

    Slight modification to evalProg and exec

--- Final/final.hs
+++ Final/final.hs
@@ -5,6 +5,8 @@ type Symbol = Char
 type Alphabet = [Symbol]
 type Label = Int
 type RegCont = String
+type Input = RegCont
+type Output = RegCont
 data Instr = ADD Index Symbol | SUB Index Symbol | BRANCH Index [Label] | PRINT | HALT deriving (Eq)
 type Program = [Instr]
 type ProgCntr = Index
@@ -23,25 +25,27 @@ rvrs = [BRANCH 0 [7, 1, 4], SUB 0 'a', ADD 1 'a', BRANCH 0 [7, 1, 4], SUB 0 'b',
 position :: Symbol -> Alphabet -> Index
 position s a = head ([i | (i, char) <- zip [1 .. ] a, s == char] ++ [length a + 1])
 
-evalProg :: Program -> RegMap -> ProgCntr -> (RegMap, ProgCntr)
-evalProg p rmap i = case p !! i of
-                      ADD k s -> if member k rmap
-                                   then evalProg p (insert k ((rmap ! k) ++ (s : [])) rmap) (i + 1)
-                                   else evalProg p (insert k (s : []) rmap) (i + 1)
-                      SUB k s -> if member k rmap
-                                   then let reg = rmap ! k in
-                                     if length reg > 0 && last reg == s
-                                       then evalProg p (insert k (take (length reg - 1) reg) rmap) (i + 1)
-                                       else evalProg p rmap (i + 1)
-                                   else evalProg p (insert k (s : []) rmap) (i + 1)
-                      BRANCH k l -> if member k rmap
-                                      then let reg = rmap ! k in
-                                        if length reg == 0
-                                          then evalProg p rmap (l !! 0)
-                                          else evalProg p rmap (l !! position (last reg) alpha)
-                                      else evalProg p (insert k [] rmap) (l !! 0)
-                      PRINT -> evalProg p rmap (i + 1)
-                      HALT -> (rmap, i + 1)
+evalProg :: Program -> RegMap -> ProgCntr -> Output -> (RegMap, ProgCntr)
+evalProg p rmap i o = case p !! i of
+                        ADD k s -> if member k rmap
+                                     then evalProg p (insert k ((rmap ! k) ++ (s : [])) rmap) (i + 1) o
+                                     else evalProg p (insert k (s : []) rmap) (i + 1) o
+                        SUB k s -> if member k rmap
+                                     then let reg = rmap ! k in
+                                       if length reg > 0 && last reg == s
+                                         then evalProg p (insert k (take (length reg - 1) reg) rmap) (i + 1) o
+                                         else evalProg p rmap (i + 1) o
+                                     else evalProg p (insert k (s : []) rmap) (i + 1) o
+                        BRANCH k l -> if member k rmap
+                                        then let reg = rmap ! k in
+                                          if length reg == 0
+                                            then evalProg p rmap (l !! 0) o
+                                            else evalProg p rmap (l !! position (last reg) alpha) o
+                                        else evalProg p (insert k [] rmap) (l !! 0) o
+                        PRINT -> if member 0 rmap
+                                   then let reg = rmap ! 0 in evalProg p rmap (i + 1) (o ++ reg)
+                                   else evalProg p (insert 0 "" rmap) (i + 1) o 
+                        HALT -> (rmap, i + 1)
 
-exec :: Program -> RegCont -> RegMap
-exec p c = fst (evalProg p (insert 0 c empty) 0)
+exec :: Program -> Input -> Output
+exec p c = (fst (evalProg p (insert 0 c empty) 0 "")) ! 0
