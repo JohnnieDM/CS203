549ed68fbee012dcd9e91ec6a5c4a19f599f865f
Johnnie Chang <cchan60@ucsc.edu>
2018-05-30 16:00:06 -0700

    parser

--- Final/ParseRegister.hs
+++ Final/ParseRegister.hs
@@ -9,19 +9,9 @@ import qualified Text.ParserCombinators.Parsec.Token as Token
 
 import Data.HashMap
 
---data Stmt = Seq [Stmt]
---          | Add RExpr Char
---          | Sub RExpr Char
---          | IfEmpty RExpr Then LExpr Else LExpr Or LExpr
---          | Print
---          | Halt
---            deriving (Show)
-
---type Index = Int
 type Index = Integer
 type Symbol = Char
 type Alphabet = [Symbol]
---type Label = Int
 type Label = Integer
 type RegCont = String
 type Input = RegCont
@@ -29,7 +19,7 @@ type Output = RegCont
 type Program = [Instr]
 type ProgCntr = Index
 type RegMap = Map Index RegCont
-type ProgCnt = Int
+type ProgCnt = Integer
 
 data Instr = Seq [Instr]
            | ADD Index Symbol
@@ -39,31 +29,64 @@ data Instr = Seq [Instr]
            | HALT
              deriving (Eq)
 
+alpha :: Alphabet
+alpha = "ab"
+
+position :: Symbol -> Alphabet -> Index
+position s a = fromIntegral (head ([i | (i, char) <- zip [1 .. ] a, s == char] ++ [length a + 1]))
+
+evalProg :: Program -> RegMap -> ProgCntr -> Output -> ((RegMap, ProgCntr), Output)
+evalProg p rmap i o = case p !! fromInteger i of
+                        ADD k s -> if member k rmap
+                                     then evalProg p (insert k ((rmap ! k) ++ (s : [])) rmap) (i + 1) o
+                                     else evalProg p (insert k (s : []) rmap) (i + 1) o
+                        SUB k s -> if member k rmap
+                                     then let reg = rmap ! k in
+                                       if length reg > 0 && last reg == s
+                                         then evalProg p (insert k (take (length reg - 1) reg) rmap) (i + 1) o
+                                         else evalProg p rmap (i + 1) o
+                                     else evalProg p (insert k (s : []) rmap) (i + 1) o
+                        BRANCH k l -> if member k rmap
+                                        then let reg = rmap ! k in
+                                          if length reg == 0
+                                            then evalProg p rmap (l !! 0) o
+                                            else evalProg p rmap (l !! fromIntegral (position (last reg) alpha)) o
+                                        else evalProg p (insert k [] rmap) (l !! 0) o
+                        PRINT -> if member 0 rmap
+                                   then let reg = rmap ! 0 in evalProg p rmap (i + 1) (o ++ reg)
+                                   else evalProg p (insert 0 "" rmap) (i + 1) o
+                        HALT -> ((rmap, i + 1), o)
+
+exec :: Program -> Input -> Output
+exec p c = snd (evalProg p (insert 0 c empty) 0 "")
+
 languageDef =
-  emptyDef { Token.identStart      = char 'R'
+  emptyDef { Token.identStart      = letter
            , Token.identLetter     = alphaNum
            , Token.reservedNames   = [ "IFEMPTY"
                                      , "THEN"
                                      , "ELSE"
                                      , "OR"
                                      , "LET"
+                                     , "+="
+                                     , "-="
                                      ]
-           , Token.reservedOpNames = ["+=", "-="]
            }
 
 lexer = Token.makeTokenParser languageDef
 
-identifier = Token.identifier  lexer -- parses an identifier
-reserved   = Token.reserved    lexer -- parses a reserved name
-reservedOp = Token.reservedOp  lexer -- parses an operator
-parens     = Token.parens      lexer -- parses surrounding parenthesis:
-                                    --   parens p
-                                    -- takes care of the parenthesis and
-                                    -- uses p to parse what's inside them
-integer    = Token.integer     lexer -- parses an integer
-charLit    = Token.charLiteral lexer -- parses an char literal
-semi       = Token.semi        lexer -- parses a semicolon
-whiteSpace = Token.whiteSpace  lexer -- parses whitespace
+identifier = Token.identifier    lexer -- parses an identifier
+reserved   = Token.reserved      lexer -- parses a reserved name
+parens     = Token.parens        lexer -- parses surrounding parenthesis:
+                                       --   parens p
+                                       -- takes care of the parenthesis and
+                                       -- uses p to parse what's inside them
+integer    = Token.integer       lexer -- parses an integer
+natural    = Token.natural       lexer -- parses a natural
+charLit    = Token.charLiteral   lexer -- parses an char literal
+stringLit  = Token.stringLiteral lexer -- parses an char literal
+semi       = Token.semi          lexer -- parses a semicolon
+whiteSpace = Token.whiteSpace    lexer -- parses whitespace
 
 rmParser :: Parser Instr
 rmParser = whiteSpace >> instruction
@@ -83,34 +106,55 @@ instruction' =   ifInstr
              <|> haltInstr
              <|> printInstr
 
+--orSeires :: Parser [Label]
+--orSeries =   label <- natural
+--         <|> do label <- natural
+--             reserved "OR"
+--             orSeries
+--
+--ifInstr :: Parser Instr
+--ifInstr =
+--  do reserved "IFEMPTY"
+--     registerIdent  <- identifier
+--     let registerIndex = take (length registerIdent - 1) $ drop 1 $ registerIdent
+--     let register = read registerIndex :: Integer
+--     reserved "THEN"
+--     label1 <- natural
+--     reserved "ELSE"
+--     something
+--     return $ BRANCH register something
+
 ifInstr :: Parser Instr
 ifInstr =
   do reserved "IFEMPTY"
-     --register  <- identifier -- TODO: read identifier "R0" into integer 0
-     register  <- integer
+     registerIdent  <- identifier
+     let registerIndex = take (length registerIdent - 1) $ drop 1 $ registerIdent
+     let register = read registerIndex :: Integer
      reserved "THEN"
-     label1 <- integer
+     label1 <- natural
      reserved "ELSE"
-     label2 <- integer
+     label2 <- natural
      reserved "OR"
-     label3 <- integer
+     label3 <- natural
      return $ BRANCH register [label1, label2, label3]
 
 addInstr :: Parser Instr
 addInstr =
   do reserved "LET"
-     --register <- identifier
-     register <- integer
-     reservedOp "+="
+     registerIdent  <- identifier
+     let registerIndex = take (length registerIdent - 1) $ drop 1 $ registerIdent
+     let register = read registerIndex :: Integer
+     reserved "+="
      symbol <- charLit
      return $ ADD register symbol
 
 subInstr :: Parser Instr
 subInstr =
   do reserved "LET"
-     --register <- identifier
-     register <- integer
-     reservedOp "-="
+     registerIdent  <- identifier
+     let registerIndex = take (length registerIdent - 1) $ drop 1 $ registerIdent
+     let register = read registerIndex :: Integer
+     reserved "-="
      symbol <- charLit
      return $ SUB register symbol
 
